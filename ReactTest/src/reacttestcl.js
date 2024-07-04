7164cabc79aee3d6eedfda0cf7a6dd96f2f2b19e
Dirga Gurung <29222621+AGRIDJAR@users.noreply.github.com>
2024-07-04 09:21:17 -0700

    Drag and drop

---
+++ ReactTest/src/reacttestcl.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+class Reactdemo extends React.Component{
+    render(){
+
+        return(
+
+            <div>
+
+                <h1>React Git project test</h1>
+            </div>
+        );
+    }
+
+}
+export default Reactdemo;
